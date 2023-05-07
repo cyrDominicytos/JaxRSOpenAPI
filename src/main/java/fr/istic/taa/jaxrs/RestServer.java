0594546fb520624868d7ac030b2ae378bdf78a44
TOSSOU Cyriaque <cyriaque.tossou@etudiant.univ-rennes1.fr>
2023-05-07 16:32:11 +0200

    add defaut user creation

--- src/main/java/fr/istic/taa/jaxrs/RestServer.java
+++ src/main/java/fr/istic/taa/jaxrs/RestServer.java
@@ -7,6 +7,11 @@ import java.util.logging.Logger;
 
 import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
 
+import fr.istic.taa.jaxrs.dao.generic.SupportDao;
+import fr.istic.taa.jaxrs.dao.generic.UserDao;
+import fr.istic.taa.jaxrs.domain.Support;
+import fr.istic.taa.jaxrs.domain.User;
+
 
 
 /**
@@ -15,7 +20,7 @@ import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
  */
 public class RestServer {
     private static final Logger logger = Logger.getLogger(RestServer.class.getName());
-
+    
     public static void main( String[] args ) {
         UndertowJaxrsServer ut = new UndertowJaxrsServer();
         TestApplication ta = new TestApplication();
@@ -24,5 +29,42 @@ public class RestServer {
                 Undertow.builder().addHttpListener(8080, "localhost")
         );
         logger.info("JAX-RS based micro-service running!");
+        
+        initDB();
+    }
+    
+    private static void initDB() {
+    	try {
+    		UserDao uDao = new UserDao();
+    		
+    		//Check if the default user is in the db
+    		User defaultUser = uDao.findByEmailAndPassword("user@gmail.com","User@2023");
+    		if(defaultUser==null)
+    		{
+    			//ajouter un utilisateur par défaut
+    			User user = new User();
+    			user.setEmail("user@gmail.com");
+    			user.setPassword("User@2023");
+    			uDao.save(user);
+    			System.out.println("Default user created successfully");
+    		}
+    		
+    		//check if the default support (it has the full control on the system) is in the db 
+    		User defaultSupport = uDao.findByEmailAndPassword("support@gmail.com","Support@2023");
+    		if(defaultSupport==null) {
+    			SupportDao sDao = new SupportDao();
+    			Support support = new Support();
+    			support.setEmail("support@gmail.com");
+    			support.setPassword("Support@2023");
+    			sDao.save(support);
+    			System.out.println("Default support created successfully");
+    		}
+    		
+    	}catch(Exception e) {
+    		System.out.println(e.getMessage());
+    	}
+    	
     }
 }
+
+
